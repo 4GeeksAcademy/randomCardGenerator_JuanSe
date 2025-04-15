23d4bfb20fd01c946c93f815ef6ee4770aff29ac
JuanSeDelgado <73569662+JuanSeDelgado@users.noreply.github.com>
2025-04-15 16:24:52 +0000

    First Commit randomCardGenerator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-import "bootstrap";
-import "./style.css";
 
+window.onload = function () {
+  console.log("Random Card Generator")
+  let values = ['A', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+  let suits = ['♠', '♥', '♦', '♣']
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let cardNumber = Math.floor(Math.random() * values.length);
+  let suitNumber = Math.floor(Math.random() * suits.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let suitValue = suits[suitNumber];
+  let cardValue = values[cardNumber];
+
+  if (suitValue == '♥' || suitValue == '♦') {
+    let cardValueColor = document.getElementById("cardValue");
+    cardValueColor.classList.add("text-danger");
+  }
+
+  let cardValueElement = document.getElementById("cardValue");
+  cardValueElement.innerHTML = cardValue;
+  cardValueElement.classList.add((suitValue === '♥' || suitValue === '♦') ? 'text-danger' : 'text-dark');
+
+  let suitElements = document.querySelectorAll(".suitValue");
+  suitElements.forEach(el => {
+    el.innerHTML = suitValue;
+    el.classList.add((suitValue === '♥' || suitValue === '♦') ? 'text-danger' : 'text-dark');
+  });
+
+}
+
